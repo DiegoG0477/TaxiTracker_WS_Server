8278f52ad58c5099143e2d318f56c6553160096b
DiegoG0477 <diego.14.13.lopez@gmail.com>
2024-07-25 14:06:37 -0600

    without express

--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,20 @@
-import express from 'express';
 import dotenv from 'dotenv';
-import * as http from 'http';
+import { createServer } from 'http';
 import { Server } from 'socket.io';
 import { WebSocketHandler } from './handler/socket.handler';
-import { socketioAuthMiddleware } from './middlewares/auth.middleware';
-import cors from 'cors';
 
 dotenv.config();
-
 const PORT = process.env.PORT ?? 3000;
 
-const app = express();
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin', 'Access-Control-Allow-Headers', 'x-access-token', 'Origin', 'X-Requested-With', 'Accept', 'access_token' ],
-}));
-
-const server = http.createServer(app);
+const server = createServer();
 
 const io = new Server(server, {
   cors: {
-      origin: '*'
+    origin: '*'
   },
   pingInterval: 1000,
   pingTimeout: 2000
 });
-
-// io.use(socketioAuthMiddleware);
 
 const websocketHandler = new WebSocketHandler(io);
 
